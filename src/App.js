a17de519d8da6642b88b08a5ea5b016fbabb5a3e
Drew <34400283+drewbdoo@users.noreply.github.com>
2022-04-14 15:13:58 -0400

    updating with 1.1 and 1.2 exercises

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+//Exercise 1.1
 
-function App() {
+// import React from "react";
+
+// const App = () => {
+//   const Header = (props) => {
+//     return <h1>{props.course}</h1>;
+//   };
+//   const Content = (props) => {
+//     return (
+//       <p>
+//         {props.name} {props.total}
+//       </p>
+//     );
+//   };
+//   const Total = (props) => {
+//     return <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>;
+//   };
+//   const course = "Half Stack application development";
+//   const part1 = "Fundamentals of React";
+//   const exercises1 = 10;
+//   const part2 = "Using props to pass data";
+//   const exercises2 = 7;
+//   const part3 = "State of a component";
+//   const exercises3 = 14;
+
+//   return (
+//     <div>
+//       <Header course={course} />
+//       <Content name={part1} total={exercises1} />
+//       <Content name={part2} total={exercises2} />
+//       <Content name={part3} total={exercises3} />
+
+//       <Total />
+//     </div>
+//   );
+// };
+
+// export default App;
+
+
+///Exercise 1.2
+import React from "react";
+
+const App = () => {
+  const Header = (props) => {
+    return <h1>{props.course}</h1>;
+  };
+
+  const Part = (props) => {
+    return(
+      <p>{props.part} {props.total}</p>
+      )
+  };
+
+const Content = () => {
+  return(
+    <div>
+      <Part part={part1} total={exercises1}/>
+      <Part part={part2} total={exercises2}/>
+      <Part part={part3} total={exercises3}/>
+    </div>
+  )
+}
+  const Total = (props) => {
+    return <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>;
+  };
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header course={course} />
+     <Content/>
+
+      <Total />
     </div>
   );
-}
+};
 
 export default App;
+
+
