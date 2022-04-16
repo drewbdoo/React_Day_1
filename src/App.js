a9d8df04acc51ef1f9e37f418170dd39c1322388
Drew <drewbdoo@gmail.com>
2022-04-16 18:00:47 -0400

    saving progress

--- src/App.js
+++ src/App.js
@@ -298,83 +298,158 @@
 
 //Exercise 1.7 and 1.8 kinda mixed together, 1.9, 1.10. and 1.11 are comment on the lines where they were added
 
-import { useState } from 'react'
+// import { useState } from 'react'
 
-//Exercise 1.10
-const StatisticLine = (props) => {
-  return (
-    //Exercise 1.11
-    <tr>
-      <td>{props.name}</td>
-      <td>{props.num}</td>
-    </tr>
-  );
-};
-const Statistics = (props) => {
-  //Exercise 1.9
-  if (props.total === 0) {
-    return (<p>No feedback given</p>)
-  }
-  return (
-    <div>
-      <h1>statistics</h1>
-      {/* Exercise 1.11 cont. */}
-      <table>
-        <tbody>
+// //Exercise 1.10
+// const StatisticLine = (props) => {
+//   return (
+//     //Exercise 1.11
+//     <tr>
+//       <td>{props.name}</td>
+//       <td>{props.num}</td>
+//     </tr>
+//   );
+// };
+// const Statistics = (props) => {
+//   //Exercise 1.9
+//   if (props.total === 0) {
+//     return (<p>No feedback given</p>)
+//   }
+//   return (
+//     <div>
+//       <h1>statistics</h1>
+//       {/* Exercise 1.11 cont. */}
+//       <table>
+//         <tbody>
 
-          <StatisticLine name="good " num={props.good} />
-          <StatisticLine name="neutral " num={props.neutral} />
-          <StatisticLine name="bad " num={props.bad} />
-          <StatisticLine name="all" num={props.total} />
-          <StatisticLine name="average" num={props.average} />
-          <StatisticLine name="positive" num={props.positive + "%"} />
-        </tbody>
-      </table>
-    </div>
-  )
-}
+//           <StatisticLine name="good " num={props.good} />
+//           <StatisticLine name="neutral " num={props.neutral} />
+//           <StatisticLine name="bad " num={props.bad} />
+//           <StatisticLine name="all" num={props.total} />
+//           <StatisticLine name="average" num={props.average} />
+//           <StatisticLine name="positive" num={props.positive + "%"} />
+//         </tbody>
+//       </table>
+//     </div>
+//   )
+// }
+// const Button = ({ onClick, text }) => (
+//   <button onClick={onClick}>
+//     {text}
+//   </button>
+// )
+// const App = () => {
+//   const Header = () => <h1>give feedback</h1>
+//   const [good, setGood] = useState(0)
+//   const [neutral, setNeutral] = useState(0)
+//   const [bad, setBad] = useState(0)
+//   const total = good + neutral + bad
+//   const average = () => {
+//     let ave = (good - bad)
+//     let overAll = (ave / total)
+//     return Math.round(overAll * 100)/100;
+//   }
+//   const positive = () => {
+//     return Math.round(1000 * good / total)/10;
+
+//   }
+//   const statProps = {
+//     good: good,
+//     bad: bad,
+//     neutral: neutral,
+//     total: total,
+//     average: average(),
+//     positive: positive(),
+//   }
+//   const handleGoodClick = () =>
+//     setGood(good + 1)
+//   const handleNeutralClick = () =>
+//     setNeutral(neutral + 1)
+//   const handleBadClick = () =>
+//     setBad(bad + 1)
+//   return (
+//     <div>
+//       <Header />
+//       <Button onClick={handleGoodClick} text='good' />
+//       <Button onClick={handleNeutralClick} text='neutral' />
+//       <Button onClick={handleBadClick} text='bad' />
+//       <Statistics {...statProps} />
+//     </div>
+//   )
+// }
+// export default App
+
+//Exercise 1.12
+
+import React, { useState } from 'react'
+
 const Button = ({ onClick, text }) => (
   <button onClick={onClick}>
-    {text}
-  </button>
-)
-const App = () => {
-  const Header = () => <h1>give feedback</h1>
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const total = good + neutral + bad
-  const average = () => {
-    let ave = (good - bad)
-    let overAll = (ave / total)
-    return Math.round(overAll * 100)/100;
-  }
-  const positive = () => {
-    return Math.round(1000 * good / total)/10;
+  {text}
+  </button>)
 
-  }
-  const statProps = {
-    good: good,
-    bad: bad,
-    neutral: neutral,
-    total: total,
-    average: average(),
-    positive: positive(),
-  }
-  const handleGoodClick = () =>
-    setGood(good + 1)
-  const handleNeutralClick = () =>
-    setNeutral(neutral + 1)
-  const handleBadClick = () =>
-    setBad(bad + 1)
+//Exercise 1.13, step 2
+const VoteCount = ({text, vote}) => (
+  <div>
+    <p>{text}</p>
+    <p>has {vote} votes</p>
+  </div>
+)
+
+
+
+
+
+
+
+const App = () => {
+
+
+  
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
+  ]
+  const [selected, setSelected] = useState(0)
+  const [vote, setVote] = useState(Array(anecdotes.length).fill(0))
+  
+ 
+  //step 1
+  const handleClick = () => {
+    setSelected(Math.floor(Math.random() * anecdotes.length))
+  };
+
+//1.13, step 2
+
+const handleVote = () => {
+  const newVotes = [...vote]
+  newVotes[selected] += 1
+  setVote(newVotes)
+  console.log(vote)
+
+}
+
+
+
+
+   
+  
+
   return (
     <div>
-      <Header />
-      <Button onClick={handleGoodClick} text='good' />
-      <Button onClick={handleNeutralClick} text='neutral' />
-      <Button onClick={handleBadClick} text='bad' />
-      <Statistics {...statProps} />
+         <VoteCount text={anecdotes[selected]} vote={vote[selected]} />
+        <div>
+      <Button onClick={handleClick} text='next anecdote'/>
+      <Button onClick={handleVote} text='vote' />
+     
+    </div>
     </div>
   )
 }
+
 export default App
