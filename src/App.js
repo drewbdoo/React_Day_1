0166d3eb9c006aa84cdfa482bf53c7e73c8afa8d
Drew <drewbdoo@gmail.com>
2022-04-18 09:08:55 -0400

    final product

--- src/App.js
+++ src/App.js
@@ -34,8 +34,6 @@
 // };
 // export default App;
 
-
-
 ///Exercise 1.2
 
 // import React from "react";
@@ -76,8 +74,6 @@
 //   );
 // };
 // export default App;
-
-
 
 // Exercise 1.3
 
@@ -127,8 +123,6 @@
 //   );
 // };
 // export default App;
-
-
 
 //Exercise 1.4
 
@@ -187,8 +181,6 @@
 //   );
 // };
 // export default App;
-
-
 
 //Exervice 1.5
 
@@ -250,8 +242,6 @@
 // };
 // export default App;
 
-
-
 //Exercise 1.6
 
 // import { useState } from 'react'
@@ -293,8 +283,6 @@
 //   )
 // }
 // export default App
-
-
 
 //Exercise 1.7 and 1.8 kinda mixed together, 1.9, 1.10. and 1.11 are comment on the lines where they were added
 
@@ -381,75 +369,62 @@
 
 //Exercise 1.12
 
-import React, { useState } from 'react'
-
-const Button = ({ onClick, text }) => (
-  <button onClick={onClick}>
-  {text}
-  </button>)
-
+import React, { useState } from "react";
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
 //Exercise 1.13, step 2
-const VoteCount = ({text, vote}) => (
+const VoteCount = ({ text, vote }) => (
   <div>
     <p>{text}</p>
     <p>has {vote} votes</p>
   </div>
-)
-
-
-
-
-
-
-
-const App = () => {
-
-
-  
-  const anecdotes = [
-    'If it hurts, do it more often',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
-  ]
-  const [selected, setSelected] = useState(0)
-  const [vote, setVote] = useState(Array(anecdotes.length).fill(0))
-  
- 
-  //step 1
-  const handleClick = () => {
-    setSelected(Math.floor(Math.random() * anecdotes.length))
-  };
-
-//1.13, step 2
-
-const handleVote = () => {
-  const newVotes = [...vote]
-  newVotes[selected] += 1
-  setVote(newVotes)
-  console.log(vote)
-
-}
-
-
-
-
-   
-  
-
+);
+//Exercise 1.14
+const HighestVote = ({ anecdotes, vote }) => {
+  const high = Math.max(...vote);
+  const highWord = vote.indexOf(high);
+  if (high === 0) {
+    return <p>Please vote for something</p>;
+  }
   return (
     <div>
-         <VoteCount text={anecdotes[selected]} vote={vote[selected]} />
-        <div>
-      <Button onClick={handleClick} text='next anecdote'/>
-      <Button onClick={handleVote} text='vote' />
-     
-    </div>
+    <h1>Anecdote with the most votes</h1>
+    <VoteCount text={anecdotes[highWord]} vote={high} />
     </div>
   )
-}
-
-export default App
+};
+const App = () => {
+  const anecdotes = [
+    "If it hurts, do it more often",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients",
+  ];
+  const [selected, setSelected] = useState(0);
+  const [vote, setVote] = useState(Array(anecdotes.length).fill(0));
+  //step 1
+  const handleClick = () => {
+    setSelected(Math.floor(Math.random() * anecdotes.length));
+  };
+  //1.13, step 2
+  const handleVote = () => {
+    const newVotes = [...vote];
+    newVotes[selected] += 1;
+    setVote(newVotes);
+    console.log(vote);
+  };
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      <VoteCount text={anecdotes[selected]} vote={vote[selected]} />
+      <div>
+        <Button onClick={handleClick} text="next anecdote" />
+        <Button onClick={handleVote} text="vote" />
+        <HighestVote anecdotes={anecdotes} vote={vote} />
+      </div>
+    </div>
+  );
+};
+export default App;
